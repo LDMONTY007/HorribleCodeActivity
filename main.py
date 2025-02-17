0c9188992298a75a356913c62138721a9947549b
LDMONTY007 <logan.d.m.117@gmail.com>
2025-02-17 15:05:18 -0500

    Created a basic caclulator script in main.py

--- main.py
+++ main.py
@@ -8,9 +8,56 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+def perform_operation(num1, operator, num2):
+    if operator == "+":
+        return add(num1, num2)
+    elif operator == "-":
+        return subtract(num1, num2)
+    elif operator == "*":
+        return multiply(num1, num2)
+    elif operator == "/":
+        return divide(num1, num2)
+    else:
+        #if they didn't enter a valid operator
+        #inform the user.
+        return str(operator) + " is not a valid operator"
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print("Welcome to the calculator, enter 'exit' to exit. ")
+
+    while True:
+        num1 = input("Please enter a number: ")
+        if num1 == "exit":
+            break
+        operator = input("Please enter an operator (+, -, *, /): ")
+        if operator == "exit":
+            break
+        num2 = input("Please enter another number: ")
+        if operator == "exit":
+            break
+
+        #try to to cast the number input to a float
+        #will throw an error if the numbers are strings.
+        try:
+            output = perform_operation(float(num1), operator, float(num2))
+            print(str(num1) + " " + str(operator) + " " + str(num2) + " = " + str(output))
+        except ValueError:
+            #print the operator that was incorrect when it was entered
+            #wrong.
+            print(output)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
